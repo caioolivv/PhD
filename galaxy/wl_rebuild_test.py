19753a14e58abcaa9e8a4ead159521a8fb2787d9
Caio Oliveira <caiolimadeoliveira@proton.me>
2023-10-30 10:29:58 -0300

    Some stuff

--- galaxy/wl_rebuild_test.py
+++ galaxy/wl_rebuild_test.py
@@ -26,7 +26,7 @@ import matplotlib.pyplot as plt
 plt.rcParams['text.latex.preamble'] = [r'\usepackage{pxfonts, mathpazo}']
 plt.rc('text', usetex=True)
 
-# np.random.seed(0)
+np.random.seed(10)
 # Define cosmological parameters
 cosmo = Cosmology(H0 = 70.0, Omega_dm0 = 0.27 - 0.045, Omega_b0 = 0.045, Omega_k0 = 0.0)
 
@@ -37,56 +37,13 @@ ngals         = 10000 # Number of galaxies
 Delta         = 200   # Overdensity parameter definition NFW profile
 cluster_ra    = 0.0   # Cluster right ascension
 cluster_dec   = 0.0   # Cluster declination
-shapenoise    = 5e-2  # True ellipticity standard variation
-photoz_noise  = 5e-2
-ndata         = 10000
+sigma_g       = 5e-2  # True ellipticity standard variation
+sigma_z       = 1e-2
+ndata         = 1000
+scale         = 1e-6
 
-# Create galaxy catalog and Cluster object
 
-def create_nc_data_cluster_wll (theta, g_t, z_source, z_cluster, cosmo, dist, sigma_z = None, sigma_g = None):
-    r = clmm.convert_units (theta, "radians", "Mpc", redshift = z_cluster, cosmo = cosmo)
-    obs = Ncm.Matrix.new (len (theta), 3)
-
-    for i in range (len (theta)):
-        obs.set (i, 0, r[i])
-        obs.set (i, 1, z_source[i])
-        obs.set (i, 2, g_t[i])
-
-    # gsdp  = Nc.GalaxySDPositionFlat ()
-    gsdp  = Nc.GalaxySDPositionSRDY1 ()
-    gsdzp = Nc.GalaxySDZProxyGauss ()
-    gsds  = Nc.GalaxySDShapeGauss ()
-
-    gsdp.set_z_lim (Ncm.Vector.new_array ([0.3, 4.1]))
-    gsdp.set_r_lim (Ncm.Vector.new_array ([0.6, 3.2]))
-    gsdzp.set_z_lim (Ncm.Vector.new_array ([0.4, 4.2]))
-    gsdzp.set_sigma (sigma_z)
-    gsds.set_sigma (sigma_g)
-
-    gwll = Nc.GalaxyWLLikelihood (s_dist=gsds, zp_dist=gsdzp, rz_dist=gsdp)
-    gwll.set_obs (obs)
-    gwll.set_ndata (ndata)
-    gwll.set_cut (z_cluster + 0.1, 4.0, 0.75, 3.0)
-
-    ga = Ncm.ObjArray.new ()
-    ga.add (gwll)
-
-    dcwll = Nc.DataClusterWLL (galaxy_array=ga, z_cluster=z_cluster)
-    dcwll.set_init (True)
-    
-    return dcwll, gwll
-
-def create_fit_obj (data_array, mset):
-    dset = Ncm.Dataset.new ()
-    for data in data_array:
-        dset.append_data (data)
-    lh = Ncm.Likelihood.new (dset)
-    fit = Ncm.Fit.new (Ncm.FitType.NLOPT, "ln-neldermead", lh, mset, Ncm.FitGradType.NUMDIFF_FORWARD)
-
-    return fit
-
-# np.random.seed(10)
-
+# Create Model Set
 moo = clmm.Modeling (massdef='mean', delta_mdef=200, halo_profile_model='nfw')
 moo.set_cosmo(cosmo)
 mset = moo.get_mset ()
@@ -98,23 +55,47 @@ mset.param_set_ftype (MDelta_pi.mid, MDelta_pi.pid, Ncm.ParamType.FREE)
 mset.param_set_ftype (cDelta_pi.mid, cDelta_pi.pid, Ncm.ParamType.FREE)
 mset.prepare_fparam_map ()
 
-data = mock.generate_galaxy_catalog (cluster_m, cluster_z, concentration, cosmo, "chang13", zsrc_min = cluster_z + 0.1, shapenoise=shapenoise, ngals=ngals, cluster_ra=cluster_ra, cluster_dec=cluster_dec, photoz_sigma_unscaled=photoz_noise)
+# Create data
+data = mock.generate_galaxy_catalog (cluster_m, cluster_z, concentration, cosmo, "chang13", zsrc_min = cluster_z + 0.1, shapenoise=sigma_g, ngals=ngals, cluster_ra=cluster_ra, cluster_dec=cluster_dec, photoz_sigma_unscaled=sigma_z)
 gc = clmm.GalaxyCluster("CL_noisy_z", cluster_ra, cluster_dec, cluster_z, data)
 gc.compute_tangential_and_cross_components(geometry="flat")
+r = clmm.convert_units (gc.galcat['theta'], "radians", "Mpc", redshift = cluster_z, cosmo = cosmo)
 
-ggt, gwll = create_nc_data_cluster_wll (gc.galcat['theta'], gc.galcat['et'], gc.galcat['z'], cluster_z, cosmo, cosmo.dist, sigma_z=photoz_noise, sigma_g=shapenoise)
-fit = create_fit_obj ([ggt], mset)
+obs = Ncm.Matrix.new (len (gc.galcat['theta']), 3)
 
-kde = gwll.peek_kde ()
-# dist = NCM_STATS_DIST (kde)
+for i in range (len (gc.galcat['theta'])):
+    obs.set (i, 0, r[i])
+    obs.set (i, 1, gc.galcat['z'][i])
+    obs.set (i, 2, gc.galcat['et'][i])
 
-r = np.linspace (0.6, 3.2, 1000)
-p = []
+gsdp  = Nc.GalaxySDPositionSRDY1 ()
+gsdzp = Nc.GalaxySDZProxyGauss ()
+gsds  = Nc.GalaxySDShapeGauss ()
 
-for i in range (1):
-    data = Ncm.Vector.new (3)
-    data.set (0, r[i])
-    data.set (1, 1.5)
-    data.set (2, 0.02)
+gsdp.set_z_lim (Ncm.Vector.new_array ([0.4, 4.5]))
+gsdp.set_r_lim (Ncm.Vector.new_array ([0.4, 4.5]))
+gsdzp.set_z_lim (Ncm.Vector.new_array ([0.4, 4.5]))
+gsdzp.set_sigma (sigma_z)
+gsds.set_sigma (sigma_g)
 
-    p.append (kde.eval (data))
+gwll = Nc.GalaxyWLLikelihood (s_dist=gsds, zp_dist=gsdzp, rz_dist=gsdp)
+gwll.set_obs (obs)
+gwll.set_ndata (ndata)
+gwll.set_cut (cluster_z + 0.1, 4.0, 0.75, 3.0)
+gwll.set_scale_cut (scale)
+
+ga = Ncm.ObjArray.new ()
+ga.add (gwll)
+
+dcwll = Nc.DataClusterWLL (galaxy_array=ga, z_cluster=cluster_z)
+dcwll.set_init (True)
+
+dset = Ncm.Dataset.new ()
+dset.append_data (dcwll)
+lh = Ncm.Likelihood.new (dset)
+fit = Ncm.Fit.new (Ncm.FitType.NLOPT, "ln-neldermead", lh, mset, Ncm.FitGradType.NUMDIFF_FORWARD)
+
+fit.run (Ncm.FitRunMsgs.SIMPLE)
+fit.obs_fisher ()
+fit.log_info ()
+fit.log_covar ()
