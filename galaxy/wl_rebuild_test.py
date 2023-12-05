d4c2f78d722a085a82c61c802f704b747aad01ee
Caio Oliveira <caiolimadeoliveira@proton.me>
2023-12-05 13:51:06 -0300

    Updated for NumCosmo version 0.19

--- galaxy/wl_rebuild_test.py
+++ galaxy/wl_rebuild_test.py
@@ -2,23 +2,16 @@ import os
 import sys
 import gi
 
-gi.require_version('NumCosmo', '1.0')
-gi.require_version('NumCosmoMath', '1.0')
-from gi.repository import GObject
-from gi.repository import NumCosmo as Nc
-from gi.repository import NumCosmoMath as Ncm
-os.environ['CLMM_MODELING_BACKEND'] = 'nc'
+from numcosmo_py import Nc
+from numcosmo_py import Ncm
 
 __name__ = "NcContext"
 
 Ncm.cfg_init ()
 Ncm.cfg_set_log_handler (lambda msg: sys.stdout.write (msg) and sys.stdout.flush ())
 
-import clmm
 import numpy as np
 from numpy import random
-from clmm import Cosmology
-from clmm.support import mock_data as mock
 from chainconsumer import ChainConsumer
 import matplotlib.pyplot as plt
 
@@ -26,17 +19,16 @@ import matplotlib.pyplot as plt
 plt.rc('text', usetex=True)
 
 np.random.seed(0)
+rng = Ncm.RNG.seeded_new("mt19937", 1)
 
 # Define cosmological parameters
-cosmo_clmm = Cosmology(H0 = 70.0, Omega_dm0 = 0.27 - 0.045, Omega_b0 = 0.045, Omega_k0 = 0.0)
+# cosmo_clmm = Cosmology(H0 = 70.0, Omega_dm0 = 0.27 - 0.045, Omega_b0 = 0.045, Omega_k0 = 0.0)
 
 cluster_m     = 10**(14.301) # Cluster mass
 cluster_z     = 0.4          # Cluster redshift
 concentration = 4            # Concentrion parameter NFW profile
 ngals         = 10000        # Number of galaxies
 Delta         = 200          # Overdensity parameter definition NFW profile
-cluster_ra    = 0.0          # Cluster right ascension
-cluster_dec   = 0.0          # Cluster declination
 sigma_g       = 5e-2         # True ellipticity standard variation
 sigma_z       = 5e-2         # True redshift standard variation
 ndata         = 1000         # Number of data points on KDE
@@ -44,39 +36,13 @@ prec          = 14           # -log10 of the integral precision
 
 is_kde        = False        # Use KDE or not
 
-moo = clmm.Modeling (massdef='mean', delta_mdef=200, halo_profile_model='nfw')
-moo.set_cosmo(cosmo_clmm)
-mset = moo.get_mset ()
-
-MDelta_pi = mset.param_get_by_full_name ("NcHaloDensityProfile:log10MDelta")
-cDelta_pi = mset.param_get_by_full_name ("NcHaloDensityProfile:cDelta")
-
-mset.param_set_ftype (MDelta_pi.mid, MDelta_pi.pid, Ncm.ParamType.FREE)
-mset.param_set_ftype (cDelta_pi.mid, cDelta_pi.pid, Ncm.ParamType.FREE)
-mset.prepare_fparam_map ()
-
-# Create data
-data = mock.generate_galaxy_catalog (cluster_m, cluster_z, concentration, cosmo_clmm, "chang13", zsrc_min = cluster_z + 0.1, shapenoise=sigma_g, ngals=ngals, cluster_ra=cluster_ra, cluster_dec=cluster_dec, photoz_sigma_unscaled=sigma_z)
-gc = clmm.GalaxyCluster("CL_noisy_z", cluster_ra, cluster_dec, cluster_z, data)
-gc.compute_tangential_and_cross_components(geometry="flat")
-r = clmm.convert_units (gc.galcat['theta'], "radians", "Mpc", redshift = cluster_z, cosmo = cosmo_clmm)
-
-obs = Ncm.Matrix.new (len (gc.galcat['theta']), 3)
-
-for i in range (len (gc.galcat['theta'])):
-    obs.set (i, 0, r[i])
-    obs.set (i, 1, gc.galcat['z'][i])
-    obs.set (i, 2, gc.galcat['et'][i])
-    # obs.set (i, 2, gc.galcat['ex'][i])
-
-cosmo = Nc.HICosmo.new_from_name(Nc.HICosmo, "NcHICosmoLCDM")
+cosmo = Nc.HICosmoLCDM.new()
 dp    = Nc.HaloDensityProfileNFW.new(Nc.HaloDensityProfileMassDef.CRITICAL, 200)
 dist  = Nc.Distance.new(10)
 
 dist.prepare(cosmo)
 
 smd = Nc.WLSurfaceMassDensity.new(dist)
-rng = Ncm.RNG.seeded_new("mt19937", 1)
 
 gsdp  = Nc.GalaxySDPositionSRDY1()
 gsdzp = Nc.GalaxySDZProxyGauss()
@@ -111,9 +77,10 @@ dcwll = Nc.DataClusterWLL (galaxy_array=ga, z_cluster=cluster_z)
 dcwll.set_init (True)
 dcwll.set_kde (is_kde)
 
-moo = clmm.Modeling (massdef='critical', delta_mdef=200, halo_profile_model='nfw')
-moo.set_cosmo(cosmo_clmm)
-mset = moo.get_mset ()
+mset = Ncm.MSet.empty_new()
+mset.set(cosmo)
+mset.set(dp)
+mset.set(smd)
 
 MDelta_pi = mset.param_get_by_full_name ("NcHaloDensityProfile:log10MDelta")
 cDelta_pi = mset.param_get_by_full_name ("NcHaloDensityProfile:cDelta")
@@ -125,9 +92,9 @@ mset.prepare_fparam_map ()
 dset = Ncm.Dataset.new ()
 dset.append_data (dcwll)
 lh = Ncm.Likelihood.new (dset)
-fit = Ncm.Fit.new (Ncm.FitType.NLOPT, "ln-neldermead", lh, mset, Ncm.FitGradType.NUMDIFF_FORWARD)
+fit = Ncm.Fit.factory (Ncm.FitType.NLOPT, "ln-neldermead", lh, mset, Ncm.FitGradType.NUMDIFF_FORWARD)
 
-fit.run (Ncm.FitRunMsgs.SIMPLE)
+fit.run (Ncm.FitRunMsgs.FULL)
 fit.obs_fisher ()
 fit.log_info ()
 fit.log_covar ()
@@ -158,7 +125,6 @@ esmcmc.end_run ()
 
 mcat = esmcmc.peek_catalog ()
 
-
 rows = np.array([mcat.peek_row(i).dup_array() for i in range(nwalkers * 400, mcat.len())])
 params = ["$" + mcat.col_symb(i) + "$" for i in range (mcat.ncols())]
 
@@ -171,8 +137,6 @@ CC_fig = partial.plotter.plot(figsize=(8, 8), truth=[4, 14.301])
 fig = plt.figure(num=CC_fig, figsize=(8,8), dpi=300, facecolor="white")
 
 if is_kde:
-    plt.title(f"KDE, {ndata} data points, {nwalkers} walkers")
-    plt.savefig(f"Plots/wl_rebuild_kde_{ndata}_{nwalkers}.png", bbox_inches='tight', dpi=300)
+    fig.savefig(f"Plots/wl_rebuild_kde_{ndata}_{nwalkers}.png", bbox_inches='tight')
 else:
-    plt.title(f"Integral, {prec} precision, {nwalkers} walkers")
-    plt.savefig(f"Plots/wl_rebuild_integral_{prec}_{nwalkers}.png", bbox_inches='tight', dpi=300)
+    fig.savefig(f"Plots/wl_rebuild_integral_{prec}_{nwalkers}.png", bbox_inches='tight')
